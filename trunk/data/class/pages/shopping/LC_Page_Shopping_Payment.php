8bf5b4b15ca9026e66f2ab5daf1b07cfb172bd06
ec@cuore.jp <ec@cuore.jp@38d0c520-a743-11de-bea2-1db044c32224>
2009-10-13 09:02:53 +0000

    送付先画面省略処理

--- trunk/data/class/pages/shopping/LC_Page_Shopping_Payment.php
+++ trunk/data/class/pages/shopping/LC_Page_Shopping_Payment.php
@@ -93,7 +93,12 @@ class LC_Page_Shopping_Payment extends LC_Page {
             $this->tpl_login = '1';
             $this->tpl_user_point = $this->objCustomer->getValue('point');
             //戻り先URL
-            $this->tpl_back_url = URL_DELIV_TOP;
+            if($this->cartdown==2){
+				// ダウンロード商品のみの場合は、カート画面に戻る
+				$this->tpl_back_url = URL_CART_TOP;
+			}else{
+				$this->tpl_back_url = URL_DELIV_TOP;
+			}
         } else {
             $this->tpl_back_url = URL_SHOP_TOP . "?from=nonmember";
         }
@@ -209,6 +214,13 @@ class LC_Page_Shopping_Payment extends LC_Page {
         if($this->objCustomer->isLoginSuccess(true)) {
             $this->tpl_login = '1';
             $this->tpl_user_point = $this->objCustomer->getValue('point');
+			//戻り先URL
+            if($this->cartdown != 0){
+				// ダウンロード商品が１つでも注文されている場合 カートに戻る
+				$this->tpl_back_url = MOBILE_URL_CART_TOP;
+			}
+        } else {
+            $this->tpl_back_url = MOBILE_URL_SHOP_TOP;
         }
 
         // 金額の取得 (購入途中で売り切れた場合にはこの関数内にてその商品の個数が０になる)
